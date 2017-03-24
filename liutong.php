4bd44b0339c68c808f256dea15a88d8908c1dfc2
ivantalk02011 <459819401@qq.com>
2017-03-24 11:21:03 +0800

    Update liutong.php

--- liutong.php
+++ liutong.php
@@ -1,17 +1,15 @@
-<?php
 $urls = array(
-'http://nyhtc.cn',
+    'http://www.nyhtc.cn/1.html',
 );
 $api = 'http://data.zz.baidu.com/urls?site=nyhtc.cn&token=A5xMG3q1OW2V5oI6';
 $ch = curl_init();
-$options = array(
-CURLOPT_URL => $api,
-CURLOPT_POST => true,
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_POSTFIELDS => implode("\n", $urls),
-CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+$options =  array(
+    CURLOPT_URL => $api,
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POSTFIELDS => implode("\n", $urls),
+    CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
 );
 curl_setopt_array($ch, $options);
 $result = curl_exec($ch);
 echo $result;
-?>
