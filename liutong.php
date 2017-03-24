98d70aad7275420c3281e2292ff90695229b2238
ivantalk02011 <459819401@qq.com>
2017-03-24 11:27:41 +0800

    Update liutong.php

--- liutong.php
+++ liutong.php
@@ -1,5 +1,5 @@
 $urls = array(
-    'http://www.nyhtc.cn/html',
+    'http://www.nyhtc.cn/index.html',
 );
 $api = 'http://data.zz.baidu.com/urls?site=nyhtc.cn&token=A5xMG3q1OW2V5oI6';
 $ch = curl_init();
